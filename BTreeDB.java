4f23a651547e3c932be8ff744840e5a489b49f9c
paolo-ortiz <paolo.ortiz@obf.ateneo.edu>
2018-11-18 16:57:58 +0800

    Added ability to get input

--- BTreeDB.java
+++ BTreeDB.java
@@ -8,11 +8,19 @@ public class BTreeDB {
 		try {
 			RandomAccessFile file = new RandomAccessFile(args[0], "rwd");
 			RandomAccessFile file2 = new RandomAccessFile(args[1], "rwd");
+
+			Scanner sc = new Scanner(System.in);
+			while (sc.hasNext()) {
+
+				String input = sc.nextLine();
+
+				if (input.equals("exit"))
+					file.close();
+					file2.close();
+					return;
+			}
 		} catch (IOException e) {
 			System.out.println("IOException in main");
 		}
-
-	}
-		
-		
+	}	
 }
