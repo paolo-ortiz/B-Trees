33082e65f3b81f528d4ff5b6bd3469ec8cd97840
paolo-ortiz <paolo.ortiz@obf.ateneo.edu>
2018-11-19 15:24:59 +0800

    starting insert, applying scanner changes

--- BTreeDB.java
+++ BTreeDB.java
@@ -13,24 +13,34 @@ public class BTreeDB {
 			while (sc.hasNext()) {
 
 				String input = sc.nextLine(); //get input
-				String[] inputArr = splitInput(input); //split input
 
+				//for getting the inputs
+				Scanner rd = new Scanner(input);
+				String command = rd.next();
 
 				//if input is "exit", close program
-				if (inputArr[0].equals("exit")) {
+				if (command.equals("exit")) {
 					file.close();
 					file2.close();
 					return;
 				}
+
+				long key = rd.nextLong();
+				String value = rd.nextLine();
+
+
+				
 				//else if input is "insert", check if it is valid
 				//first, make sure input has more than 1 value
-				else if (inputArr[0].equals("insert") && inputArr.length > 1) {
+				if (command.equals("insert")) {
+
 					//if first value is an integer AND there is no string after
 					//FOR NOW, ASSUME USER ALWAYS INPUTS CORRECT INPUT
-					if (inputArr.length == 2) {
-						insert(inputArr[1], "");
-						System.out.println("Inserted");
-					}
+					
+					insert(key, value);
+
+					//else if there is more than 2 elements
+
 				}
 				//else if invalid command, print "invalid command"
 				else
@@ -43,9 +53,9 @@ public class BTreeDB {
 	}	
 
 	//inserts value into tree
-	public static void insert(Object hash, String word) {
+	public static void insert(long key, String word) {
 		
-		System.out.printf("Inserted %s", word);
+		System.out.printf("Inserted %s\n", word);
 	}
 
 	//splits input into array to get values
