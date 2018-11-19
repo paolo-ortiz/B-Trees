21a3fd5501b2e451361343be207c9e9ebe0e6a84
paolo-ortiz <paolo.ortiz@obf.ateneo.edu>
2018-11-19 15:50:34 +0800

    new class for handling insert

---
+++ ValuesManager.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class ValuesManager {
+
+
+	//Constructor
+	ValuesManager(String name) throws IOException {
+
+		RandomAccessFile file2 = new RandomAccessFile(name, "rwd");
+
+		//check if file exists
+		
+	}
+
+
+
+}
