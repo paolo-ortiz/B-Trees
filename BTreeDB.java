121e281b2195b8b490bd20f0d17827c4b228030f
ReynerFeelsyou <45160235+ReynerFeelsyou@users.noreply.github.com>
2018-11-26 20:39:26 +0800

    Add files via upload
    Fixed blank inputs, only BTreeDB was modified which is why it is the only file here to show up

--- BTreeDB.java
+++ BTreeDB.java
@@ -7,11 +7,12 @@ public class BTreeDB {
 
 		try {
 			
-			RandomAccessFile file = new RandomAccessFile(args[0], "rwd");
+			//BTreeManager btm = new BTreeManager(args[0]);
 			ValuesManager vm = new ValuesManager(args[1]);
 
 			Scanner sc = new Scanner(System.in);
 			while (sc.hasNext()) {
+				String value;
 
 				String input = sc.nextLine(); //get input
 
@@ -21,7 +22,7 @@ public class BTreeDB {
 
 				//if input is "exit", close program
 				if (command.equals("exit")) {
-					file.close();
+					btm.closeData();
 					vm.closeData();
 					//file2.close();
 					return;
@@ -30,15 +31,34 @@ public class BTreeDB {
 				//else if input is "insert", check if it is valid
 				//first, make sure input has more than 1 value
 				if (command.equals("insert")) {
-
+					
 					long key = rd.nextLong();
-					String value = rd.nextLine().trim();
+					
+					if(rd.hasNextLine() == true) {
+						
+					 value = rd.nextLine().trim();	//checks if there is any more input 
+					}
+					
+					
+					else {
+						
+					 value = ""; 	//if no input is found, then a blank is added instead
+					}
+					
 					
 					insertToVal(key, value, vm);
 
 					//else if there is more than 2 elements
 
 				}
+
+				//insert to node dapat to
+				else if (command.equals("insertTest")) {
+					// insert to node will  a key but will mess with value index somewhere
+					//btm.insertToNode();
+				}
+
+
 				//else if invalid command, print "invalid command"
 				else
 					System.out.println("INVALID COMMAND");
