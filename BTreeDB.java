608109e236503e5b36d2460cf26d5b27d602c78d
ReynerFeelsyou <45160235+ReynerFeelsyou@users.noreply.github.com>
2018-12-02 15:50:46 +0800

    added "update" command, and "UpdateString" method

--- BTreeDB.java
+++ BTreeDB.java
@@ -7,14 +7,15 @@ public class BTreeDB {
 
 		try {
 			
-	
-
 			BTreeManager btm = new BTreeManager(args[0]);
 			ValuesManager vm = new ValuesManager(args[1]);
-
+			//BTreeNode does not have getNode 
+			//BTreeNode bn = btm.getNode();
+			//temp index
+			int index = 0;
 			Scanner sc = new Scanner(System.in);
+			
 			while (sc.hasNext()) {
-				String value;
 
 				String input = sc.nextLine(); //get input
 
@@ -26,44 +27,58 @@ public class BTreeDB {
 				if (command.equals("exit")) {
 					btm.closeData();
 					vm.closeData();
-					//file2.close();
 					return;
 				}
 				
 				//else if input is "insert", check if it is valid
 				//first, make sure input has more than 1 value
 				if (command.equals("insert")) {
-					
+
 					long key = rd.nextLong();
-					
-					if(rd.hasNextLine() == true) {
-						
-					 value = rd.nextLine().trim();	//checks if there is any more input 
-					}
-					
-					
-					else {
-						
-					 value = ""; 	//if no input is found, then a blank is added instead
-					}
-					
-					
-					insertToVal(key, value, vm);
+					String value = rd.nextLine().trim();
 
-					//else if there is more than 2 elements
+					//check if key already exists
+					boolean exists = btm.isPresent(key);
+					//if it does not exist, then insert
+					if (exists == false) {
+						insertToVal(value, vm);
+						insertToBT(key,index, btm);
+						System.out.printf("%d inserted\n", key);
+					} else 
+						System.out.printf("ERROR: %d already exists\n", key);	
+
+					
 
 				}
 
-				//insert to node dapat to
-				else if (command.equals("insertTest")) {
-					// insert to node will  a key but will mess with value index somewhere
-					//btm.insertToNode();
-				}
+				else if(command.equals("select")) {
 
+					long key = rd.nextLong(); //gets key from command
+
+					int valueIndex = btm.getValueIndex(key); //get value index
+
+					select(key, valueIndex, vm);
+
+				}
+				else if (command.equals("update")) {
+					long key = rd.nextLong();	//gets key
+					int valueIndex = btm.getValueIndex(key); //gets index of what key is pointing to
+					String value = rd.nextLine().trim(); //the updated string 
+					
+					if(valueIndex != -1) {	//if there is a key, then it will update
+						vm.updateString(valueIndex, value);
+						System.out.printf("%d updated\n", key);	
+					}
+					
+					else 
+						System.out.printf("ERROR: %d does not exist\n", key);
+				
+					
+				}
 
 				//else if invalid command, print "invalid command"
 				else
-					System.out.println("INVALID COMMAND");
+					System.out.println("ERROR: Invalid Command");
 
 			}
 		} catch (IOException e) {
@@ -71,13 +86,22 @@ public class BTreeDB {
 		}
 	}	
 
-	//inserts value into tree
-	public static void insertToVal(long key, String word, ValuesManager vm) throws IOException {
-		
-		System.out.printf("Inserted %s\n", word);
-		vm.insert(key, word);
-
+	//inserts value into data.val
+	public static void insertToVal(String word, ValuesManager vm) throws IOException {
+		vm.insert(word);
 	}
 
-	
+	//inserts value into data.bt
+	public static void insertToBT(long key, int index, BTreeManager btm) throws IOException {
+		btm.insertToNode(key,index);
+	}
+
+	public static void select(long key, int valueIndex, ValuesManager vm) throws IOException {
+
+		if (valueIndex != -1) { //a value of -1 means key is not in btree
+			String value = vm.getString(valueIndex); //get string
+			System.out.printf("%d ==> %s\n", key, value);
+		} else 
+			System.out.printf("ERROR: %d does not exist\n", key);
+	}
 }
